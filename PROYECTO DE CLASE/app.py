915938a121c5018b1416405f002e3802e34b5e18
jaroveva1831-svg <jaroveva1831@gmail.com>
2026-05-11 19:45:02 -0500

    Prueba de envio

---
+++ PROYECTO DE CLASE/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+import requests
+from io import StringIO
+
+
+
+
+ruta_zni = 'https://github.com/juliandariogiraldoocampo/analisis_taltech/raw/refs/heads/main/explorador/ZNI.csv'
+response = requests.get(ruta_zni, verify=False)
+
+df = pd.read_csv(StringIO(response.text))
+
+st.header('Estado de la Prestación del Servicio de Energía')
+st.subheader('Zonas no Interconectadas (ZNI)')
+st.text('Conjunto de Datos')
+
+st.dataframe(df)
+
+print("Hola mundo")
